54180a3b0e7e871309ce14ee7f6c53ee9c5bc007
Bálek Miroslav <mirbalek@seznam.cz>
2023-10-13 16:16:26 +0200

    Add ldap parsing and bind

--- ldap.c
+++ ldap.c
@@ -7,7 +7,7 @@
  * @author xbalek02 Miroslav Bálek
  *
  *
- *  Last modified: Oct 3, 2023
+ *  Last modified: Oct 13, 2023
  *
  */
 #include <stdio.h>
@@ -23,46 +23,134 @@
 #include <unistd.h>
 #include "ldap.h"
 
-int MAX_BUFFER_SIZE = 2048;
+// represents offeset pointer to revecied data
+int currentTagPosition;
 
-int ldap_parse_request(LdapRequest *request, unsigned char *data, size_t length)
+int ldap_parse_request(unsigned char *data, size_t length)
 {
 
-    if (data[0] != 0x30)
+    if (data[0] != LDAP_MESSAGE_PREFIX)
     {
+        printf("ERROR Received not an ldap message\n");
         return -1;
     }
-    request->message_id = data[4];
+    if (length < 5)
+        return -1;
 
-    if (data[5] == 0x60)
+    unsigned char requestOperation = data[5];
+
+    switch (requestOperation)
     {
-        printf("Bind message received");
+    case LDAP_BIND_REQUEST:
+        printf("Received a Bind Request.\n");
+        ldap_bind(data);
+        break;
+    case LDAP_SEARCH_REQUEST:
+        printf("Received a Search Request.\n");
+        // ldap_search();
+        break;
+    case LDAP_MODIFY_REQUEST:
+        printf("Received a Modify Request.\n");
+        break;
+
+    case LDAP_UNBIND_REQUEST:
+        printf("Received a Unbind Request.\n");
+        break;
+
+        // TODO: Implement rest of operations
+
+    default:
+        printf("Received an unknown or unsupported operation.\n");
     }
 
     return 0;
 }
 
-void ldap(int client_socket)
+void ldap_bind(unsigned char *data)
 {
-    size_t receivedBytes;
-    unsigned char *receivedData = ldap_receive(client_socket, &receivedBytes);
-    printHexMessage(receivedData, receivedBytes);
-    LdapRequest request;
+    LdapBind bind;
+    // TODO: define as constants
+    currentTagPosition = 7;
+    bind.version = get_int_value(data);
+    // TODO handle error response if version is different from 3
 
-    if (ldap_parse_request(&request, receivedData, receivedBytes) == -1)
-    {
-        printf("Error parsing \n");
-        free(receivedData);
-        return;
-    }
-
-    printf("%d", request.message_id);
-
-    // Free the allocated memory for receivedData
-    free(receivedData);
+    get_string_value(data);
+    printf("Version: %d\n", bind.version);
 }
 
-void printHexMessage(const unsigned char *data, size_t length)
+void ldap_response()
+{
+}
+
+LdapElementInfo get_ldap_element_info(unsigned char *data)
+{
+    LdapElementInfo elementInfo;
+    elementInfo.lengthOfData = data[currentTagPosition + 1];
+    elementInfo.start = currentTagPosition + 2;
+    elementInfo.end = elementInfo.start + elementInfo.lengthOfData - 1;
+    elementInfo.nextTagPosition = currentTagPosition + elementInfo.lengthOfData + 2;
+    elementInfo.tagValue = data[currentTagPosition];
+
+    print_ldap_element_info(elementInfo);
+    return elementInfo;
+}
+
+void print_ldap_element_info(LdapElementInfo elementInfo)
+{
+
+    printf("Tag Value: %02X\n", elementInfo.tagValue);
+    printf("Length of Data: %d\n", elementInfo.lengthOfData);
+    printf("Start: %d\n", elementInfo.start);
+    printf("End: %d\n", elementInfo.end);
+    printf("New Tag Position: %d\n\n", elementInfo.nextTagPosition);
+}
+
+void get_string_value(unsigned char *data)
+{
+    LdapElementInfo LdapInfo = get_ldap_element_info(data);
+
+    if (LdapInfo.tagValue != OCTET_STRING) // TODO: end correctly
+        printf("Unexpected element type");
+
+    for (int i = LdapInfo.start; i <= LdapInfo.end; i++)
+    {
+        printf("%c", data[i]);
+    }
+    printf("\n");
+    ldap_next_element(LdapInfo);
+}
+
+void ldap_next_element(LdapElementInfo LdapInfo)
+{
+    currentTagPosition = LdapInfo.nextTagPosition;
+}
+
+long long get_int_value(unsigned char *data)
+{
+
+    LdapElementInfo LdapInfo = get_ldap_element_info(data);
+
+    if (LdapInfo.tagValue != INTEGER) // TODO: end correctly
+        printf("Unexpected element type");
+    // Extract the integer value
+    long long combinedDecimalNumber = 0;
+
+    if (LdapInfo.end != LdapInfo.start)
+    {
+        for (int i = LdapInfo.start; i <= LdapInfo.end; i++)
+        {
+            combinedDecimalNumber = combinedDecimalNumber * 256 + data[i];
+        }
+    }
+    else
+    {
+        combinedDecimalNumber = data[LdapInfo.start];
+    }
+    ldap_next_element(LdapInfo);
+    return combinedDecimalNumber;
+}
+
+void print_hex_message(const unsigned char *data, size_t length)
 {
     for (size_t i = 0; i < length; i++)
     {
@@ -71,13 +159,13 @@ void printHexMessage(const unsigned char *data, size_t length)
     printf("\n");
 }
 
-unsigned char *ldap_receive(int client_socket, size_t *receivedBytes)
+unsigned char *ldap_receive(int clientSocket, size_t *receivedBytes)
 {
     unsigned char buffer[MAX_BUFFER_SIZE];
     int bytesReceived;
 
     // Receive data into the buffer using recv
-    bytesReceived = recv(client_socket, buffer, sizeof(buffer), 0);
+    bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
 
     if (bytesReceived < 0)
     {
@@ -100,4 +188,21 @@ unsigned char *ldap_receive(int client_socket, size_t *receivedBytes)
     *receivedBytes = bytesReceived;
 
     return receivedData;
+}
+
+void ldap(int clientSocket)
+{
+    size_t receivedBytes;
+
+    unsigned char *receivedData = ldap_receive(clientSocket, &receivedBytes);
+    print_hex_message(receivedData, receivedBytes);
+
+    if (ldap_parse_request(receivedData, receivedBytes) == -1)
+    {
+        printf("Error parsing \n");
+        free(receivedData);
+        return;
+    }
+    // Free the allocated memory for receivedData
+    free(receivedData);
 }
--- ldap.h
+++ ldap.h
@@ -7,7 +7,7 @@
  * @author xbalek02 Miroslav Bálek
  *
  *
- *  Last modified: Oct 3, 2023
+ *  Last modified: Oct 13, 2023
  *
  *
  */
@@ -15,16 +15,42 @@
 #ifndef LDAP_H
 #define LDAP_H
 
-extern int MAX_BUFFER_SIZE;
+int LDAP_MESSAGE_PREFIX = 0x30;
 
-enum ProtocolOp
+enum LDAPPrtotocolOp
 {
-    BIND_REQUEST,
-    BIND_RESPONSE,
-    UNBIND_REQUEST,
-    SEARCH_REQUEST,
-    SEARCH_RESULT_ENTRY,
-    SEARCH_RESULT_DONE,
+    LDAP_BIND_REQUEST = 0x60,         // 96
+    LDAP_BIND_RESPONSE = 0x61,        // 97
+    LDAP_UNBIND_REQUEST = 0x42,       // 66
+    LDAP_SEARCH_REQUEST = 0x63,       // 99
+    LDAP_SEARCH_RESPONSE = 0x64,      // 100
+    LDAP_MODIFY_REQUEST = 0x66,       // 102
+    LDAP_MODIFY_RESPONSE = 0x67,      // 103
+    LDAP_ADD_REQUEST = 0x68,          // 104
+    LDAP_ADD_RESPONSE = 0x69,         // 105
+    LDAP_DELETE_REQUEST = 0x4A,       // 74
+    LDAP_DELETE_RESPONSE = 0x6B,      // 107
+    LDAP_MODIFY_DN_REQUEST = 0x6C,    // 108
+    LDAP_MODIFY_DN_RESPONSE = 0x6D,   // 109
+    LDAP_COMPARE_REQUEST = 0x6E,      // 110
+    LDAP_COMPARE_RESPONSE = 0x6F,     // 111
+    LDAP_ABANDON_REQUEST = 0x50,      // 80
+    LDAP_SEARCH_RESULT_ENTRY = 0x64,  // 100
+    LDAP_SEARCH_RESULT_DONE = 0x65,   // 101
+    LDAP_EXTENDED_REQUEST = 0x77,     // 119
+    LDAP_EXTENDED_RESPONSE = 0x78,    // 120
+    LDAP_INTERMEDIATE_RESPONSE = 0x79 // 121
+};
+
+enum TagType
+{
+    EOC = 0x00,
+    BOOLEAN = 0x01,
+    INTEGER = 0x02,
+    BIT_STRING = 0x03,
+    OCTET_STRING = 0x04,
+    NULL_TYPE = 0x05,
+    MAX_BUFFER_SIZE = 2048
 };
 
 enum ResultCode
@@ -35,16 +61,144 @@ enum ResultCode
     SIZE_LIMIT_EXCEEDED = 4,
 };
 
+/**
+ * Structure representing an LDAP Bind message.
+ *
+ * The LdapBind structure is used to represent an LDAP Bind message, containing
+ * essential information about the message, including the message ID and the protocol version.
+ */
 typedef struct
 {
-    int message_id;
-    enum ProtocolOp operation;
-    unsigned char baseObject[255];
-} LdapRequest;
+    int message_id; /**< The unique identifier for the LDAP Bind message. */
+    int version;    /**< The protocol version associated with the LDAP Bind message. */
+} LdapBind;
+
+/**
+ * Structure representing the result of an LDAP operation.
+ *
+ * The LDAPResult structure is used to store the result of an LDAP operation,
+ * including a result code, matched distinguished name (DN), and diagnostic message.
+ */
+typedef struct
+{
+    uint8_t resultCode;          /**< The result code indicating the outcome of the LDAP operation. */
+    char matchedDN[128];         /**< The distinguished name (DN) that matched the LDAP operation. Adjust size as needed. */
+    char diagnosticMessage[128]; /**< A diagnostic message providing additional information. Adjust size as needed. */
+} LDAPResult;
+
+/**
+ * Structure representing the response to an LDAP Bind operation.
+ *
+ * The BindResponse structure is used to represent the response to an LDAP Bind operation,
+ * including an LDAP result and an optional field for server SASL credentials.
+ */
+typedef struct
+{
+    LDAPResult ldapResult;     /**< The result of the LDAP Bind operation. */
+    char serverSaslCreds[128]; /**< Optional server SASL credentials. Adjust size as needed. */
+} BindResponse;
+
+/**
+ * Structure representing LDAP element information.
+ *
+ * The LdapElementInfo structure is used to store detailed information about an LDAP element.
+ * It contains various properties related to the element's position, data length, and tag value.
+ */
+typedef struct
+{
+    int start;              /**< The start position of value in the LDAP element. */
+    int end;                /**< The end position of value in the LDAP element. */
+    int lengthOfData;       /**< The length of value in the LDAP element. */
+    int nextTagPosition;    /**< The position of the next LDAP tag in the data. */
+    unsigned char tagValue; /**< The tag value associated with the LDAP element. */
+} LdapElementInfo;
 
 void ldap(int client_socket);
+
+/**
+ * Receive LDAP data from a client socket.
+ *
+ * This function receives LDAP data from the specified client socket
+ * and stores it in a buffer. It also updates the receivedBytes variable
+ * with the number of bytes received.
+ *
+ * @param client_socket The socket to receive data from.
+ * @param receivedBytes A pointer to a size_t variable to store the number of received bytes.
+ *
+ * @return A pointer to the received data (a dynamically allocated buffer).
+ *         The caller is responsible for freeing this buffer.
+ */
 unsigned char *ldap_receive(int client_socket, size_t *receivedBytes);
-void printHexMessage(const unsigned char *data, size_t length);
-int ldap_parse_request(LdapRequest *request, unsigned char *data, size_t length);
+
+/**
+ * Parse an LDAP request to determine the LDAP operation.
+ *
+ * This function analyzes the provided LDAP request data to determine the type
+ * of LDAP operation it represents, such as bind, search, modify, etc.
+ *
+ * @param data The LDAP request data to be parsed.
+ * @param length The length of the LDAP request data.
+ *
+ * @return 0 if the LDAP request is successfully parsed and represents a valid LDAP operation.
+ *         A non-zero value is returned if an error occurs during parsing.
+ */
+int ldap_parse_request(unsigned char *data, size_t length);
+
+/**
+ * Move to the next element in the LDAP data structure and update the global variable.
+ *
+ * This function is used to navigate through the elements in an LDAP data structure
+ * in a sequential manner. It also updates the
+ * global variable `currentTagPosition` based on the `LdapInfo.nextTagPosition` value.
+ *
+ * @param LdapInfo LdapElementInfo structure to get information about new tag position.
+ */
+void ldap_next_element(LdapElementInfo LdapInfo);
+
+void ldap_search();
+void ldap_bind();
+void ldap_unbind();
+
+/**
+ * Print a hexadecimal representation of received data.
+ *
+ * This function prints the hexadecimal representation of received data to the standard
+ * output. It is useful for displaying received data in a hexadecimal format.
+ *
+ * @param data   A pointer to the received data to be printed in hexadecimal.
+ * @param length The length of the received data in bytes.
+ */
+void print_hex_message(const unsigned char *data, size_t length);
+
+/**
+ * Print LDAP element information to the standard output.
+ *
+ * This function is used to display detailed information about an LDAP element,
+ * including its start position, end position, length of data, next tag position,
+ * and tag value, to the standard output.
+ *
+ */
+void print_ldap_element_info(LdapElementInfo elementInfo);
+
+/**
+ * Extract an integer value from received data at the element position indicated by 'currentTagPosition'.
+ *
+ * This function retrieves an integer value from the received data at the element position specified
+ * by the global variable 'currentTagPosition' and returns it as a long long integer.
+ *
+ * @param data A pointer to the data containing the integer value.
+ * @return The extracted integer value as a long long integer.
+ */
+long long get_int_value(unsigned char *data);
+
+/**
+ * Extract a string value from received data at the element position indicated by 'currentTagPosition'.
+ *
+ * This function retrieves a string value from the received data at the element position specified
+ * by the global variable 'currentTagPosition' and performs some action with the string data.
+ *
+ * @param data A pointer to the data containing the string value.
+ */
+void get_string_value(unsigned char *data);
 
 #endif
